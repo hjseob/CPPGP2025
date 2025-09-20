674fea4519b9c9691c41ecd09558bea04af081bc
JinseobHan <jinsuphan1@gmail.com>
2025-09-20 12:40:04 +0900

    refactor: minor refactoring

--- CPPGP2025.cpp
+++ CPPGP2025.cpp
@@ -3,29 +3,16 @@
 
 #include <iostream>
 #include "ZVector3.h"
+#include "ZMatrix.h"
 
 int main()
 {
-    ZVector3 v1(1, 1, 1);
-    std::cout << v1 << std::endl;
+	ZVector3 v1(1, 1, 1);
+	std::cout << v1 << std::endl;
 
-    ZVector3 v2(-1, 1, 1);
-    std::cout << v2.cross(v1).normalized() << std::endl;
+	ZVector3 v2(-1, 1, 1);
+	std::cout << v2.Cross(v1) << std::endl;
 
-	std::cout << v1.normalized().dot(v2.normalized()) << std::endl;
-
-
-
-
+	std::cout << v1.Dot(v2) << std::endl;
+	
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
